f9ee29601c2975285b766a6d49073593a3f40133
Monique Bennett <54629018+monmon1994@users.noreply.github.com>
2020-03-12 08:55:41 +0200

    Update TidyT_10_03_2020.R

--- TidyT_10_03_2020.R
+++ TidyT_10_03_2020.R
@@ -5,17 +5,11 @@ library(viridis)
 library(ggthemes)
 library(wesanderson)
 
-tuition_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_cost.csv')
-
-tuition_income <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_income.csv') 
-
 salary_potential <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/salary_potential.csv')
 
-historical_tuition <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/historical_tuition.csv')
-
-diversity_school <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/diversity_school.csv')
-
 # Twitter Figure
+
+s_abb <- data.frame(state.name, state.abb) # US states
 
 salary_potential <- merge(salary_potential, s_abb, by.x = "state_name", by.y = "state.name")
 
